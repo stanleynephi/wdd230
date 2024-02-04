c2dd5955173efe94ef6a8dd75c8888847552bac5
stanleynephi <nul23001@byui.edu>
2024-02-04 17:54:57 +0000

    updates on chamber of commerce site

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -14,3 +14,12 @@ const showmodified = modifieddate.getDate()
 
 lastmod.innerHTML = `Last Modified:`+` `+`${modifieddate.toLocaleDateString("en-UK", option)}`;
 
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
+
