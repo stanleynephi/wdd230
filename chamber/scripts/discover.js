3438b2b11d0ce9165c7ace9ee11d95e5257b5e89
stanleynephi <nul23001@byui.edu>
2024-03-05 16:11:45 +0000

    week07 chamber

--- chamber/scripts/discover.js
+++ chamber/scripts/discover.js
@@ -8,41 +8,54 @@ const option = {
     second: "numeric",
 }
 
+
+
 const date = new Date().toLocaleDateString(undefined,option) 
 
 function setLastVisit(){
     localStorage.setItem("lastVisit", Date.now())
 }
-
 setLastVisit();
+
+
+let visitsmade = Number(window.localStorage.getItem("Visitsmade-ls")) || 0;
+visitsmade++;
+localStorage.setItem("Visitsmade-ls",visitsmade)
+
+    
 
 function displayDaysSinceLastVisit() {
 
-  const lastVisit = localStorage.getItem('lastVisit');
-  if (lastVisit) {
-      const lastVisited = parseInt(lastVisit);
-      const currentTime = Date.now();
-      const timeDifference = currentTime - lastVisited;
-      const oneDayInMilliseconds = 24 * 60 * 60 * 1000; // 1 day in milliseconds
+const lastVisit = localStorage.getItem('lastVisit');
+const currentTime = Date.now();
+const oneDayInMilliseconds = 24 * 60 * 60 * 1000; // 1 day in milliseconds
 
-      
-      if (timeDifference < oneDayInMilliseconds) {
-          return "Back so soon! Awesome!";
-      } else {
-          const daysDifference = Math.floor(timeDifference / oneDayInMilliseconds);
-          if (daysDifference === 1) {
-              return "You last visited 1 day ago.";
-          } else {
-              return "You last visited " + daysDifference + " days ago.";
-          }
-      }
-  } else {
-      return "Welcome! Let us know if you have any questions.";
-  }
+
+
+if (visitsmade === 1) {
+    localStorage.setItem('lastVisit', currentTime);
+    return "Welcome! This is your first time on the website.";
+} else {
+    // Calculate the time difference between the current visit and the last visit
+    const lastVisited = parseInt(lastVisit);
+    const timeDifference = currentTime - lastVisited;
+    
+    if (timeDifference < oneDayInMilliseconds) {
+        let visits = parseInt(localStorage.getItem('Visits')) || 0;
+        if (visits === 1) {
+            visits++;
+            localStorage.setItem('Visit', visits);
+        }
+        return "Back So Soon"
+    } else {
+        return "Welcome back! It's been more than a day since your last visit.";
+    }
+}
+
 }
 
 
-// displayDaysSinceLastVisit()
-const visit = document.querySelector(".visit").innerHTML = displayDaysSinceLastVisit();
+displayDaysSinceLastVisit()
+const visit = document.querySelector(".visited").innerHTML = displayDaysSinceLastVisit();
 
 
