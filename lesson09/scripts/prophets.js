060dc3bdc32991a22d7229b976f4a5d0f9baca12
stanleynephi <nul23001@byui.edu>
2024-03-16 20:06:34 +0000

    week 09 assignment

--- lesson09/scripts/prophets.js
+++ lesson09/scripts/prophets.js
@@ -4,8 +4,59 @@ const cards = document.querySelector("#cards");
 
 async function getProphetData(){
     const response = await fetch(url)
-    const data = response.json();
-    console.table(data.prophets);
+    const data = await response.json()
+    // const prophet = data.prophets;
+    console.table(data.prophets)
+    displayProphets(data.prophets)
 }
 
-getProphetData()
+getProphetData();
+
+
+
+
+const displayProphets = (data) => {
+    data.forEach((prophet) => {
+        let card = document.createElement("section");
+        let portrait = document.createElement("img");
+        let fullname = document.createElement("h2");
+        let dates = document.createElement("dates");
+
+        fullname.style.marginTop="60px";
+        dates.style.marginBottom="20px"
+        
+        dates.textContent = `Born on ${prophet.birthdate} and Died on ${prophet.death}`;
+        portrait.setAttribute("src", prophet.imageurl);
+        portrait.setAttribute("alt", `Portrait of ${prophet.name}`)
+        portrait.setAttribute("loading","lazy")
+        portrait.setAttribute("width","340")
+        portrait.setAttribute("height","440");
+        fullname.textContent=`${prophet.name}  ${prophet.lastname}`;
+
+        card.appendChild(fullname)
+        card.appendChild(dates)
+        card.appendChild(portrait)
+
+
+        cards.appendChild(card)
+    });
+}
+
+// function displayProphets(prophets){
+//         prophets.foreach((prophet) => {
+//             let card = document.createElement("section");
+//             let fullname = document.createElement("h2");
+//             let portrait = document.createElement("img");
+            
+//             fullname.textContent=`${prophet["name"]}`;
+//             portrait.setAttribute("src", prophet.imageurl);
+//             portrait.setAttribute("alt", `Portrait of ${prophet.name}`)
+//             portrait.setAttribute("loading","lazy")
+//             portrait.setAttribute("width","340")
+//             portrait.setAttribute("height","440");
+    
+//             card.appendChild(fullname)
+//             card.appendChild(portrait)
+        
+//     });
+// }
