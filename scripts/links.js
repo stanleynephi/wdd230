c92bb3ca644ee35fd17b3918618dca42369824f5
stanleynephi <nul23001@byui.edu>
2024-03-18 04:27:41 +0000

    json for lessons

--- scripts/links.js
+++ scripts/links.js
@@ -1,10 +1,33 @@
-const baseURL = "https://yourgithubusername.github.io/wdd230/";
-const linksURL = "https://yourgithubusername.github.io/wdd230/data/links.json";
+const baseURL = "https://stanleynephi.github.io/wdd230/";
+const linksURL = "https://stanleynephi.github.io/wdd230/data/links.json";
+const main = document.querySelector(".card")
 
 async function getLinks(){
     const response = await fetch(linksURL);
-    const data = await response.json();
-    console.table(data)
+    const weeks = await response.json();
+    console.table(weeks.lessons)
+    displayLinks(weeks.lessons);
 }
 
 getLinks();
+
+
+function displayLinks(weeks){
+    weeks.forEach((lesson) => {
+        // create a list and a elements
+        let list = document.createElement("ul");
+        lesson.links.forEach(link =>{
+            let item = document.createElement("li");
+            let urls = document.createElement("a");
+
+            urls.textContent =link.title
+            urls.href = baseURL + link.url
+
+            item.appendChild(urls)
+            list.appendChild(item)
+        })
+
+        main.appendChild(list)
+        
+    });
+}
