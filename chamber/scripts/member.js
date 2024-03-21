d84d5ef0b7266861ecb444e3696a8b8e0c33d49d
stanleynephi <nul23001@byui.edu>
2024-03-21 16:49:45 +0000

    chamber members

--- chamber/scripts/member.js
+++ chamber/scripts/member.js
@@ -1,7 +1,9 @@
 
-const addresses = document.querySelector(".directory");
+const addresses = document.querySelector(".cards");
 const baseUrl = "https://stanleynephi.github.io/wdd230/";
 const membersAPI = "https://stanleynephi.github.io/wdd230/chamber/data/members.json";
+const buttonslist = document.querySelector("#list")
+const buttonsgrid = document.querySelector("#grid")
 
 
 // fucntion to fetch the data from the api
@@ -17,7 +19,8 @@ getData()
 function populateData(companies){
     companies.forEach((member) => {
         //create a card for each
-        let card = document.createElement("p");
+        let card = document.createElement("div")
+        let image = document.createElement("img")
         let names = document.createElement("h4");
         let address1 = document.createElement("h5");
         let web = document.createElement("a");
@@ -30,23 +33,42 @@ function populateData(companies){
         address1.textContent = detail.address
         web.textContent = detail.website
         web.setAttribute('href',`${detail.website}`)
-        phone1.textContent = detail.phonenumbers.number1
+        phone1.textContent = detail.phonenumbers.number;
+        image.setAttribute("src", detail.image)
+        image.setAttribute("alt", `Portrait of ${detail.name}`)
+        image.setAttribute("loading","lazy")
+        image.setAttribute("width","340")
+        image.setAttribute("height","440");
 
-        card.style.width="30%"
-        card.style.textAlign ="center"
-        // address1.style.width = "30%"
-        // address1.style.textAlign = "center"
 
-     
-        // web.href = detail.website
+    card.appendChild(image)
+    card.appendChild(names)
+    card.appendChild(address1)
+    card.appendChild(web)  
+
+
+    card.style.margin="3%"
     })
 
-
-    phones.appendChild(phone1)
-
-    card.append(names,address1,web,phones)
-
+    
     addresses.appendChild(card)
+
     });
 
 }
+
+buttonslist.addEventListener("click", () => {
+	// example using arrow function
+	addresses.classList.add("list");
+});
+
+buttonsgrid.addEventListener("click", () => {
+	// example using arrow function
+	addresses.classList.remove("list");
+   
+});
+
+
+
+
+
